49f1687d4b42bfee79d24df65a0d29f601487627
Luca Abeni <luca.abeni@unitn.it>
2012-04-19 12:14:35 +0200

    Refactor the "stats" program a little bit
    this needs much more work

--- stats.c
+++ stats.c
@@ -9,19 +9,70 @@
 
 #define TIME_PERC 1000000
 
+static unsigned int tolerance;
+static int start_time, end_time;
+static FILE *l;
+
+static void stats_event_handle(const struct event *e)
+{
+  unsigned long int instant, pdf;
+  double cdf;
+  static unsigned int time_back;
+
+  switch (e->type) {
+    case TASK_END:
+          //Blocks
+          instant = response_time(e->task, e->time);
+          pdf = pdf_response_time(e->task, instant, tolerance);
+          cdf = cdf_response_time(e->task, pdf);
+          encod_stats_time(l, e->time, e->task, RESPONSE_TIME, instant, pdf, cdf);
+          blocks_task(e->task);
+          break;
+    case TASK_DESCHEDULE:
+          //Deschedule
+          instant = end_execution(e->task, e->time);
+          if (instant != 0) {
+            pdf = pdf_executions(e->task, instant, tolerance);
+            cdf = cdf_executions(e->task, pdf);
+            encod_stats_time(l, e->time, e->task, EXECUTION_TIME, instant, pdf, cdf);
+          }
+          break;
+    case TASK_ARRIVAL:
+          //Unblocks
+          instant = intervalls(e->task, e->time);
+          if (instant == 0) {	//first time no interval
+            encod_stats_time(l, e->time, e->task, INTERVALL_TIME, 0, 0, 0);
+          } else {
+            pdf = pdf_intervalls(e->task, instant, tolerance);
+            cdf = cdf_intervalls(e->task, pdf);
+            encod_stats_time(l, e->time, e->task, INTERVALL_TIME, instant, pdf, cdf);
+          }
+          break;
+    case TASK_SCHEDULE:
+          //Dispatched
+          start_execution(e->task, e->time);
+          break;
+    case TASK_NAME:
+          //Create
+          create_task(e->task, e->time);
+          break;
+    default:
+          fprintf(stderr, "Unknown event %d\n", e->type);
+  }
+
+  if (e->time - time_back >= TIME_PERC) {
+    time_back = e->time;
+    calculateCPUsUtil(l, TIME_PERC);
+  }
+}
+
 int main(int argc, char *argv[])
 {
     FILE *f;
     int res, done;
-    int start_time = 0, end_time = 0;
     char *fname;
 
-    unsigned int tolerance = 0, time_back;
-    FILE *l = NULL;
-
-
-l = fopen("test.txt", "w");
-
+    l = stdout;
     fname = argv[argc - 1];
     f = fopen(fname, "r");
     if (f == NULL) {
@@ -36,58 +87,16 @@ l = fopen("test.txt", "w");
 	done = feof(f) || (res < 0);
     }
 
-    time_back = 0;
     done = 0;
     while(!done) {
-      struct event *e;
-      unsigned long int instant, pdf;
-      double cdf;
+        struct event *e;
 
-      e = evt_get();
-
-      switch (e->type) {
-        case TASK_END:
-          //Blocks
-          instant = response_time(e->task, e->time);
-          pdf = pdf_response_time(e->task, instant, tolerance);
-          cdf = cdf_response_time(e->task, pdf);
-          encod_stats_time(l, e->time, e->task, RESPONSE_TIME, instant, pdf, cdf);
-          blocks_task(e->task);
-          break;
-        case TASK_DESCHEDULE:
-          //Deschedule
-          instant = end_execution(e->task, e->time);
-          if (instant != 0) {
-            pdf = pdf_executions(e->task, instant, tolerance);
-            cdf = cdf_executions(e->task, pdf);
-            encod_stats_time(l, e->time, e->task, EXECUTION_TIME, instant, pdf, cdf);
-          }
-          break;
-        case TASK_ARRIVAL:
-          //Unblocks
-          instant = intervalls(e->task, e->time);
-          if (instant == 0) {	//first time no interval
-            encod_stats_time(l, e->time, e->task, INTERVALL_TIME, 0, 0, 0);
-          } else {
-            pdf = pdf_intervalls(e->task, instant, tolerance);
-            cdf = cdf_intervalls(e->task, pdf);
-            encod_stats_time(l, e->time, e->task, INTERVALL_TIME, instant, pdf, cdf);
-          }
-          break;
-	case TASK_SCHEDULE:
-          //Dispatched
-          start_execution(e->task, e->time);
-          break;
-        case TASK_NAME:
-          //Create
-          create_task(e->task, e->time);
-          break;
-      }
-
-      if (e->time - time_back >= TIME_PERC) {
-        time_back = e->time;
-        calculateCPUsUtil(l, TIME_PERC);
-      }
+        e = evt_get();
+        if (e == NULL) {
+            done = 1;
+        } else {
+            stats_event_handle(e);
+        }
     }
 
     return 0;
