656172b7bc1857d21c0a3341b63f0ddd8be0168b
Luca Abeni <luca.abeni@unitn.it>
2012-05-04 16:17:23 +0200

    Add a streaming example

---
+++ Scripts/stream_example.sh
@@ -0,0 +1,11 @@
+DEBUGFS=/mnt
+TRACINGFS=$DEBUGFS/tracing
+
+echo 0 > $TRACINGFS/tracing_enabled
+#cat $TRACINGFS/trace_pipe > /dev/null
+
+echo "sched_switch sched_wakeup sched_migrate_task" > $TRACINGFS/set_event
+
+echo 1 > $TRACINGFS/tracing_enabled
+
+cat $TRACINGFS/trace_pipe | ./import - 2> /dev/null | ./visual -
