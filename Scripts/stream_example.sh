fad898ce9b2bf21ab0a99c39fa933e6f4492c84e
Luca Abeni <luca.abeni@unitn.it>
2012-05-05 00:19:02 +0200

    Fix the stream_example script after the "no stdin" modification in "visual"

--- Scripts/stream_example.sh
+++ Scripts/stream_example.sh
@@ -8,4 +8,6 @@ echo "sched_switch sched_wakeup sched_migrate_task" > $TRACINGFS/set_event
 
 echo 1 > $TRACINGFS/tracing_enabled
 
-cat $TRACINGFS/trace_pipe | ./import - 2> /dev/null | ./visual -
+mkfifo /tmp/fifo
+cat $TRACINGFS/trace_pipe | ./import - 2> log > /tmp/fifo &
+./visual /tmp/fifo 
