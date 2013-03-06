288eebdf9bbbed02c8bfcb6c210995049d5a56fe
Luca Abeni <luca.abeni@unitn.it>
2013-03-06 13:03:35 +0100

    Improve the "register" example script

--- Scripts/register_example.sh
+++ Scripts/register_example.sh
@@ -1,4 +1,28 @@
-DEBUGFS=/sys/kernel/debug
+get_debugfs() {
+  RES=$(mount | grep debugfs | cut -d ' ' -f 3)
+  if [ x$RES = x ]
+   then
+    echo >&2 DebugFS not mounted!!!
+    sudo mkdir -p /sys/kernel/debug
+    sudo mount -t debugfs debugfd /sys/kernel/debug
+    if [ $? = 0 ]
+     then
+      echo /sys/kernel/debug
+      exit 0
+     else
+      echo >&2 Cannot mount DebugFS!!! Please enable debugfs and ftrace in the kernel config!
+      exit 1
+     fi
+   fi
+  echo $RES
+}
+
+
+DEBUGFS=$(get_debugfs)
+if [ $? -eq 1 ]
+ then
+  exit 1
+ fi
 TRACINGFS=$DEBUGFS/tracing
 
 echo 0 > $TRACINGFS/tracing_enabled
